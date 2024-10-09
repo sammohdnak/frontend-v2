108e1c5c748a8a2112b294951f2a772573253590
sammohdnak <sammohdnak@gmail.com>
2024-10-10 01:01:25 +0400

    new changes

--- src/components/contextual/pages/vebal/VeBALMarketingHeader.vue
+++ src/components/contextual/pages/vebal/VeBALMarketingHeader.vue
@@ -34,7 +34,11 @@ function navigateToGetVeBAL() {
 </script>
 
 <template>
-  <div class="flex flex-col md:flex-row">
+  <div style="background-image: url('/images/backgrounds/vebal-bg.png');" class="w-full h-fit py-20 bg-cover rounded-xl gap-4 bg-no-repeat flex flex-col items-center justify-center text-white xl:mr-5">
+<p class="font-bold text-3xl">Extra earnings & voting power</p>
+<p class="font-medium text-sm">Balancer Protocol liquidity incentives are directed to pools by veBAL voters.<br/> Stake in these pools to earn incentives. Boost with veBAL for up to 2.5x extra.</p>
+  </div>
+  <!-- <div class="flex flex-col md:flex-row">
     <div class="py-8 lg:py-4 px-4 lg:px-8 2xl:px-0 xl:pt-0 max-w-md hero-text">
       <p class="mb-4 font-medium text-black dark:text-white">veBAL</p>
       <h1 class="mb-5 text-black dark:text-white title">
@@ -136,7 +140,7 @@ function navigateToGetVeBAL() {
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <style scoped>
